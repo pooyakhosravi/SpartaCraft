567cb88942286187c8d8fe84aba08eaf65504393
Pooya Khosravi <pooya32kh@gmail.com>
2019-05-20 23:57:29 -0700

    Added actionspace and enchanment

--- src/mission.py
+++ src/mission.py
@@ -10,6 +10,7 @@ import random
 import tkinter as tk
 import canvas as cnv
 import environment 
+from ActionSpace import ActionSpace
 
 try:
     import MalmoPython
@@ -17,6 +18,31 @@ except ImportError:
     import malmo.MalmoPython as MalmoPython
 
 
+
+def getPlayerState(observed):
+    is_alive = observed["IsAlive"]
+    life = observed["Life"]
+    xpos = observed["XPos"]
+    ypos = observed["YPos"]
+    zpos = observed["ZPos"]
+    pitch = observed["Pitch"]
+    yaw = observed["Yaw"]
+
+    return (is_alive, life, xpos, ypos, zpos, pitch, yaw)
+
+
+def getState(observed):
+    is_alive = observed["IsAlive"]
+    life = observed["Life"]
+    xpos = observed["XPos"]
+    ypos = observed["YPos"]
+    zpos = observed["ZPos"]
+    pitch = observed["Pitch"]
+    yaw = observed["Yaw"]
+
+    
+
+    return observed
 
 
 
@@ -73,23 +99,37 @@ if __name__ == '__main__':
     print("Mission running ", end=' ')
 
 
+    #moves = ["forward 1",  "attack 1", "attack 0",  "back 1", "moveMouse 0 100", "left 1", "right 1", "moveMouse 100 0", "moveMouse -100 0", "moveMouse 0 -100"]
+    moves = ["forward 1", "back 1", "left 1", "right 1", "attack 1", "attack 0", "moveMouse 100 0", "moveMouse -100 0"]
+    moveactions = {"forward 1": "back 0", "left 1": "right 0", "right 1": "left 0", "back 1": "forward 0"}
+    action_space = ActionSpace(moves)
 
-    moves = ["forward 1", "forward 0", "left 1", "left 0"]
+    print("Starting AGENT!!")
 
-    # agent_host.sendCommand("forward 1")
+    agent_host.sendCommand("chat /give @p diamond_sword 1 0 {ench:[{id:16,lvl:1000},{id:17,lvl:500},{id:19,lvl:300}]}")
+    agent_host.sendCommand("chat hello!")
+    agent_host.sendCommand("hotbar.2 1")
+    agent_host.sendCommand("hotbar.2 0")
+    agent_host.sendCommand("moveMouse 0 -200")
 
     # Loop until mission ends:
     while world_state.is_mission_running:
         print(".", end="")
         time.sleep(c.AGENT_TICK_RATE / 1000)
         world_state = agent_host.getWorldState()
-        if moves:
-            agent_host.sendCommand(moves.pop(0))
+
+        if True:
+            a = action_space.random()
+            if a in moveactions:
+                agent_host.sendCommand(moveactions[a])
+            agent_host.sendCommand(a)
+
+
         if world_state.number_of_observations_since_last_state > 0:
             msg = world_state.observations[-1].text
-            # print(msg)
 
             ob = json.loads(msg)
+            # print(getState(ob))
             if "entities" in ob:
                 entities = ob["entities"]
                 cnv.drawMobs(root, canvas, entities, True)
